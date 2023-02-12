86e57698325e788020fa8ef31c95f09cbaf5fbcc
|°_°| <grivel.theo@protonmail.com>
2023-02-12 04:11:44 +0100

    iter: avoid segfault

--- framework/src/utils_s_unit_test.c
+++ framework/src/utils_s_unit_test.c
@@ -2,9 +2,12 @@
 
 void iter_list(t_unit_test *testlist, void (*f)(t_unit_test *))
 {
+	t_unit_test	*next;
+
 	while (testlist)
 	{
+		next = testlist->next;
 		f(testlist);
-		testlist = testlist->next;
+		testlist = next;
 	}
 }
